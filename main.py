909ac592342ff3bd94579e3efc29de62eb8883c0
Bimba Dalaev <narkobalkon@mail.ru>
2022-12-07 12:27:53 +0300

    done

---
+++ main.py
@@ -0,0 +1,44 @@
+import itertools as I
+import operator as O
+
+
+class TreeStore:
+    def __init__(self, items):
+        self.__items = {item['id']: item for item in items}
+        self.__chlds = {pid: [item['id'] for item in items]
+                        for pid, items in I.groupby(self.__items.values(), O.itemgetter('parent'))}
+
+    def getAll(self):
+        return list(self.__items.values())
+
+    def getItem(self, ident, default=None):
+        return self.__items.get(ident, default)
+
+    def getChildren(self, ident):
+        return list(map(self.getItem, self.__chlds.get(ident, [])))
+
+    def getAllParents(self, ident):
+        parent_id = self.getItem(ident, {}).get('parent')
+        if parent_id is not None:
+            parent = self.getItem(parent_id)
+            return [parent] + self.getAllParents(parent['id'])
+        return []
+
+
+def main():
+    items = [
+        {'id': 1, 'parent': None},
+        {'id': 2, 'parent': 1, 'type': 'test'},
+        {'id': 3, 'parent': 1, 'type': 'test'},
+        {'id': 4, 'parent': 2, 'type': 'test'},
+        {'id': 5, 'parent': 2, 'type': 'test'},
+        {'id': 6, 'parent': 2, 'type': 'test'},
+        {'id': 7, 'parent': 4, 'type': None},
+        {'id': 8, 'parent': 4, 'type': None}
+    ]
+
+    ts = TreeStore(items)
+
+
+if __name__ == '__main__':
+    main()
